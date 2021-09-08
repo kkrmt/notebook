63bdac196ecd4af393c63edfe502deb891d60066
kkrmt <kurimoto.kei.721+kkrmt@gmail.com>
2021-09-08 23:47:44 +0900

    hash slice

--- rails/first.rb
+++ rails/first.rb
@@ -4,3 +4,14 @@ class StatusClass < ApplicationRecord
   STATUS_LIST = %w[draft published deleted].freeze
   validates :status, inclusion: { in: STATUS_VALUES }
 end
+
+
+# @see: https://qiita.com/yagince/items/cfb131db58190d0ebb7b
+# @see: https://mikanmarusan.hatenablog.com/entry/2017/12/28/124049
+def hoge_fug
+  ATTRIBUTES_LIST = %w[hoge fuga piyo].freeze
+  attr = attributes.slice(*ATTRIBUTES_LIST).merge(
+    hoge_str: :hoge_new,
+    fuga_int: :fuga_new,
+  )
+end
